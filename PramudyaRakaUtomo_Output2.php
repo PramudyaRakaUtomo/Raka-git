40c895307fa2c6c1d49db007596794a4e2edb859
PramudyaRakaUtomo <tukisyo1@gmail.com>
2022-08-30 20:07:38 +0700

    first commit

---
+++ PramudyaRakaUtomo_Output2.php
@@ -0,0 +1,32 @@
+<h1>Indentitas Diri</h1>
+   <?php
+	  echo "NAMA : Pramudya Raka Utomo";
+    ?>
+<br>
+   <?php
+      echo "NIM  : 210411100058";
+   ?>
+<br>
+   <?php
+      echo "TEMPAT LAHIR : Surabaya";
+   ?>
+<br>
+   <?php
+      echo "TANGGAL LAHIR : 30 Oktober 2002";
+   ?>
+<br>
+   <?php
+      echo "JENIS KELAMIN : Laki Laki";
+   ?>
+<br>
+   <?php
+      echo "ALAMAT : JL. Laut Sawu 1C BLOK 5N/1, Tunjung, Burneh, Bangkalan";
+   ?>
+<br>
+<body style="background-color: aqua;">
+
+<style type="text/css">
+	h1{
+		text-align: center;
+	}
+</style>
