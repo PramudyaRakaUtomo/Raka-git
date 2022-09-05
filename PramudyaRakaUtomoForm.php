de4def81c8faa0e9708271e6882a8348339b2ea5
PramudyaRakaUtomo <tukisyo1@gmail.com>
2022-09-05 20:33:58 +0700

    second commit

---
+++ PramudyaRakaUtomoForm.php
@@ -0,0 +1,8 @@
+<h2>Form Nama dan NIM</h2>
+<form action="PramudyaRakaUtomoSession.php" method="POST" name="Form_user">
+
+    Nama<input type="text" name="Nama"><br>
+    NIM<input type="text" name="NIM">
+<input type="submit" name="submit" value="kirim">
+<br>
+<form>
